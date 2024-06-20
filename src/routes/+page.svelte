f60e857495e8082fc8fdb7c5fd307ad0cbf20b57
AnderBEz <andersson.beaz4313@gmail.com>
2024-06-19 23:50:45 -0600

    Add: text elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <script>
     import "../app.css";
   </script>
-<h1 class="text-4xl text-orange-500">Hiram se la come</h1>
+<h1 class="text-4xl text-orange-500 font-principal">Hiram se la come</h1>
