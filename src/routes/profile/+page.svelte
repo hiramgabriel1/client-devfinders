1a3eaa3268d1b7acab6d3a5011351bf8a4ee934d
AnderBEz <andersson.beaz4313@gmail.com>
2024-06-21 14:12:42 -0600

    feature: navprofile ready

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,7 +1,18 @@
 <script>
-    import Nav from "$lib/components/Nav.svelte";
+    import Navpro from "$lib/components/Navpro.svelte";
     import "../../app.css"
 </script>
 <!-- my profile -->
 
-<Nav></Nav>
+<Navpro></Navpro>
+
+<section>
+    <div class="container py-6">
+        <div class="flex flex-col md:flex-row">
+            <div class="md:w-64">
+                
+            </div>
+            <div></div>
+        </div>
+    </div>
+</section>
