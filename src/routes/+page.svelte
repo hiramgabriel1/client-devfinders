4066f4049da51e2882f86273bcebc1361a9945df
itsrusty <rusty3031@gmail.com>
2024-06-21 20:01:36 -0700

    f

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css";
-  </script>
+    import Footer from "../components/Footer.svelte";
+
+</script>
   
 <h1 class="text-4xl text-orange-500">gafete se la come</h1>
+
+<Footer/>
