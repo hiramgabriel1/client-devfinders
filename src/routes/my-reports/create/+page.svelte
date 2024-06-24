ab38169254a3f9c25ba6fef127605579657b1a43
itsrusty <rusty3031@gmail.com>
2024-06-23 22:04:29 -0700

    feature: add routes to create and view reports

---
+++ src/routes/my-reports/create/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import "../../../app.css"
+    import { type CreateReport } from "../../../types/report.interface"
+
+    let formData: CreateReport = {
+        titleReport: ''
+    }
+</script>
+
+<!-- create new report -->
---
+++ src/types/report.interface.ts
@@ -0,0 +1,3 @@
+export interface CreateReport {
+    titleReport: string
+}
