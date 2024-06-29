45df75ca5aacd5ad65c5116ad9a0bd5fc87002d7
DeniceMorones <denice.rico4211@gmail.com>
2024-06-28 19:58:04 -0600

    Feature: Home-Page

---
+++ src/lib/components/Publicity.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import publicity from "$lib/public/images/publicity.png";
+    import "../../app.css";
+</script>
+
+<div class=" bg-slate-200 mt-4 mb-8">
+    <div>
+        <img src={publicity} alt="publicity" class="w-full" />
+    </div>
+    <div class="font-secondary text-slate-800 py-2 font-semibold px-3 lg:text-sm">
+        <p>Everything Explained - E[24]:Inflation and Unemployment</p>
+    </div>
+</div>
