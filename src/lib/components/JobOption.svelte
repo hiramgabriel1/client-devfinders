042b508f85a262ed1afe4a1373dbaeb196fe0847
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-07-18 01:50:30 -0600

    feature: job listing

---
+++ src/lib/components/JobOption.svelte
@@ -0,0 +1,41 @@
+<script>
+    import "../../app.css"
+    import jobIcon from "$lib/public/images/trabajoIcon1.png"
+    import homeIcon from "$lib/public/images/homeIcon.png"
+    import locationIcon from "$lib/public/images/location_icon.png"
+    import saveIcon from "$lib/public/images/save_b.png"
+</script>
+
+<div class="py-3">
+    <div class="lg:w-96 focus:bg-color-blue-light border rounded-lg border-gray-300 ">
+        <div class="flex justify-around items-center py-3">
+            <div>
+                <img src={jobIcon} alt="jobIcon">
+            </div>
+            <div>
+                <p class="text-lg font-semibold">Ecommerce</p>
+                <p>$110k-120k</p>
+            </div>
+            <div>
+                <p class="bg-color-secondary px-2 py-1 text-color-secondary-highlight text-xs rounded-lg">Hot</p>
+            </div>
+        </div>
+        <div class="flex lg:flex-col py-3">
+            <div class="px-10 flex justify-around lg:justify-start items-center lg:items-start space-x-3 lg:space-x-0 lg:flex-col lg:space-y-3">
+                <div class="flex items-center text-gray-300 text-xs space-x-3 space-y-2">
+                    <img src={homeIcon} alt="" class="w-5 h-5">
+                    <p>Laborum</p>
+                </div>
+                <div class="flex items-center text-gray-300 text-xs space-x-3">
+                    <img src={locationIcon} alt="" class="w-4 h-4">
+                    <p>Location</p>
+                </div>
+                <div class="flex items-center text-gray-300 text-xs space-x-3">
+                    <img src={saveIcon} alt="" class="w-4 h-4">
+                    <p>Save</p>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+</div>
--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -1,62 +1,58 @@
 <script>
     import FilterJobs from "$lib/components/common/FilterJobs.svelte";
-    import "../../app.css"
+  import Navpro from "$lib/components/common/Navpro.svelte";
+  import JobOption from "$lib/components/JobOption.svelte";
+  import placeImg from "$lib/public/images/paisaje.jpg"
+  import shareIcon from "$lib/public/images/share.png"
+  import heartIcon from "$lib/public/images/heartIcon.png"
 </script>
-
+<Navpro></Navpro>
 <FilterJobs></FilterJobs>
 <section>
-    <div class="container">
-        <div class="lg:py-4">
-          <div>
-            <p
-              class="font-secondary font-bold text-slate-600 py-2 text-s lg:text-2xl"
-            >
-              Search
-            </p>
-            <input
-              class="block bg-slate-200 w-full rounded focus:border-color-blue-highlight placeholder:text-slate-800 placeholder:text-xs px-1 placeholder:lg:text-sm lg:h-6"
-              placeholder="Designer"
-            />
-          </div>
-          <div>
-            <div class="flex justify-between">
-              <p
-                class="font-secondary font-bold text-slate-600 py-2 text-s lg:text-2xl"
-              >
-                Filter
-              </p>
-              <button
-                class=" text-xs rounded-md
-                    border-0 text-color-blue-highlight font-bold sm:text-s
-                    hover:text-color-blue-light ease-in duration-200 lg:text-sm"
-              >
-                Clear filters
-              </button>
+    <div class="container font-semibold py-4">
+        <p>32 Results for <span class="text-color-blue-highlight">'Designer'</span></p>
+    </div>
+</section>
+
+<section>
+    <div class="container py-3">
+        <div class="flex justify-around">
+            <div class="flex flex-col lg:flex-start justify-center lg:justify-start">
+                <JobOption></JobOption>
+                <JobOption></JobOption>
+                <JobOption></JobOption>
+                <JobOption></JobOption>
+                
             </div>
-    
-            <div class="flex place-content-between font-secondary">
-              <select
-                class="block bg-slate-200 rounded text-xs h-6 w-[23%] lg:h-6 lg:text-sm text-slate-600"
-              >
-                <option>Location</option>
-              </select>
-              <select
-                class="block bg-slate-200 rounded text-xs h-6 w-[23%] lg:h-6 lg:text-sm text-slate-600"
-              >
-                <option>Experience level</option>
-              </select>
-              <select
-                class="block bg-slate-200 rounded text-xs h-6 w-[23%] lg:h-6 lg:text-sm text-slate-600"
-              >
-                <option>Industry</option>
-              </select>
-              <select
-                class="block bg-slate-200 rounded text-xs h-6 w-[23%] lg:h-6 lg:text-sm text-slate-600"
-              >
-                <option>Company type</option>
-              </select>
+            <div class="border border-gray-300 rounded">
+                <div>
+                    <div class="relative w-full h-52">
+                        <img src={placeImg} alt="" class="w-full object-cover h-auto clip-half-height">
+                        <style>
+                            .clip-half-height {
+                               clip-path: inset(0 0 60% 0);
+  }
+                        </style>
+                    </div>
+                    <div class="flex justify-between items-center px-10">
+                        <div class="text-lg font-semibold">
+                            <h3>Proyecto Ecommerce</h3>
+                        </div>
+                        <div class="flex space-x-2">
+                            <button>
+                                <img src={shareIcon} alt="" class="w-4">
+                            </button>
+                            <button class="flex space-x-1 bg-color-blue-light p-2 rounded">
+                                <img src={heartIcon} alt="" class="w-4">
+                                <p class="text-color-blue-highlight text-xs">Save</p>
+                            </button>
+                            <button class="bg-color-blue-highlight text-white px-2 rounded">
+                                <p class="text-xs">Aplicar</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+    </div>
 </section>
