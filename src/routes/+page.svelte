3d06e3a8419fc0123a7f867de2a5ec9ba3f4040e
itsrusty <rusty3031@gmail.com>
2024-07-18 13:54:12 -0700

    [feat]: layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,3 @@
-<script context="module">
-  export async function load() {
-    return {
-      title: "Tu Título de Página",
-      meta: [
-        { name: "description", content: "Nuvix es algo alaburguer" },
-        { name: "keywords", content: "palabras clave, separadas, por, comas" },
-        { name: "author", content: "Tu Nombre" },
-        { property: "og:title", content: "Título para Open Graph" },
-        { property: "og:description", content: "Descripción para Open Graph" },
-        { property: "og:image", content: "URL de tu imagen" },
-        { property: "og:url", content: "URL de tu página" },
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:title", content: "Título para Twitter" },
-        { name: "twitter:description", content: "Descripción para Twitter" },
-        { name: "twitter:image", content: "URL de tu imagen para Twitter" },
-      ],
-    };
-  }
-</script>
-
 <script>
   import "../app.css";
   import FooterLanding from "$lib/components/FooterLanding.svelte";
