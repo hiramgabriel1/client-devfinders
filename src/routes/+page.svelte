30c2be1b29811c8afb909115e9036b9f3ecfcce5
itsrusty <rusty3031@gmail.com>
2024-07-18 11:21:20 -0700

    [feature]: new section beta tester

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,7 +56,7 @@
       <div class="mt-10 flex items-center justify-center gap-x-6">
         <a
           class="isomorphic-link isomorphic-link--internal inline-flex items-center justify-center gap-2 rounded-xl bg-blue-600 px-4 py-3 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-          href="/login"
+          href="/beta-tester"
           >Convertirse en beta tester
           <svg
             xmlns="http://www.w3.org/2000/svg"
