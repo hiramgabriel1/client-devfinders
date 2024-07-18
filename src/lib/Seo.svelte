e6eea5c83157f3e8974ed4112805304467f9bcb8
itsrusty <rusty3031@gmail.com>
2024-07-18 15:08:59 -0700

    image upload icon

--- src/lib/Seo.svelte
+++ src/lib/Seo.svelte
@@ -1,9 +1,11 @@
 <script>
+  import icon from "$lib/public/images/icon.jpeg"
   export let title;
   export let content;
 </script>
 
 <svelte:head>
   <title>{title}</title>
+  <link rel="shortcut icon" href={icon} type="image/x-icon">
   <meta name="description" {content} />
 </svelte:head>
--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -3,9 +3,11 @@
   import Footer from "$lib/components/Footer.svelte";
   import Introduction from "$lib/components/Introduction.svelte";
   import Iconslide from "$lib/components/Iconslide.svelte";
+  import Seo from "$lib/Seo.svelte";
   import "../../app.css";
 </script>
 
+<Seo title="Landing prueb" content="landing" />
 <Nav />
 <Introduction />
 <Iconslide />
